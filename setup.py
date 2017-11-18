68aef0abf50e39261a38bc19c8737bc389252f72
Atsushi Watanabe <atsushi.w@ieee.org>
2017-11-19 03:51:48 +0900

    Add install and exec test.

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
     url='https://github.com/at-wat/eagle2svg',
     author='Atsushi Watanabe',
     author_email='atsushi.w@ieee.org',
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
     install_requires=['xmltodict'],
-    scripts=['scripts/eagle2svg']
+    scripts=['scripts/eagle2svg'],
+    license="BSD"
 )
