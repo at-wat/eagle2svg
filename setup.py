9aa90bcfc54b9c2029c86edde8f089bb090a9dce
Atsushi Watanabe <atsushi.w@ieee.org>
2017-11-18 12:12:24 +0900

    Fix repository uri.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='eagle2svg',
     version=eagle2svg.__version__,
     description='Eagle CAD file to svg renderer',
-    url='https://github.com/at-wat/eagle-to-svg',
+    url='https://github.com/at-wat/eagle2svg',
     author='Atsushi Watanabe',
     author_email='atsushi.w@ieee.org',
     packages=find_packages(exclude=['tests*']),
